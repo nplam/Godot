a0a2eb02b6a373cb0c46d1f3679d1d4b369f5aef
nplam <nguyenphuonglam40@gmail.com>
2026-02-18 08:05:46 -0800

    Wed commit

--- learning-2d-game/main.gd
+++ learning-2d-game/main.gd
@@ -4,26 +4,25 @@ extends Node
 var score
 
 func game_over():
-	$scoreTimer.stop()
-	$MobTimer.stop()
+	$HUD.show_game_over()
 	
 func new_game():
 	score = 0
-	$Player.start($StartPosition.position)
-	$startTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 	
 func _on_mob_timer_timeout():
 	# Creat a new instance of the Mob scene
 	var mob = mob_scene.instantiate()
 	
 	# Choose a random location on Path20
-	var mob_spawn_location = $MobPath/ModSpawnLocation
+	var mob_spawn_location = $MobPath/MobSpawnLocation
 	mob_spawn_location.progress_ratio =randf()
 	
 	# Set the mob's postion to the random location
 	mob.position = mob_spawn_location.position
 	
-	# Set the mob's direction perpidencular to the path direction
+	# Set the mob's direction perpendicular to the path direction
 	var direction = mob_spawn_location.rotation + PI / 2
 	
 	# Add some randomness to the direction
@@ -38,11 +37,12 @@ func _on_mob_timer_timeout():
 	add_child(mob)
 	
 func _ready():
-	new_game()
+	pass#	new_game()
 
 func _on_score_timer_timeout():
 	score += 1
-	
+	$HUD.update_score(score)
+		
 func _on_start_timer_timeout():
 	$MobTimer.start()
 	$scoreTimer.start()
